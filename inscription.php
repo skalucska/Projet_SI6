8757ce1a6976bee209c09c505bbcc22b7a90acc7
skalucska <skalucska@gmail.com>
2020-05-11 14:13:08 +0200

    inscription demandeur

---
+++ inscription.php
@@ -0,0 +1,27 @@
+<php>
+
+<html>
+	<head>
+       <meta charset="UTF-8">
+       <b> INSCRIPTION </b>
+	<p> Veuillez remplir les informations nécessaires à l'inscription </p>
+    </head>
+
+
+	<body>
+		<form action="" method="POST">
+
+			Nom : <input type="text" name="nom"><br>
+			Prénom : <input type="text" name="prenom"><br>
+			Numéro de téléphone : <input type="number" name="tel"><br>
+            Adresse mail: <input type="text" name="mail"><br>
+            Nombre de personnes : <input type="password" name="foyer"><br>
+            Mot de passe : <input type="password" name="mdp"><br>
+            Confirmation du mot de passe : <input type="password" name="mdp"><br>
+
+
+			<input type="submit" value= "Valider l'inscription">
+		</form>
+	</body>
+</html>
+<?php>
